3960bfc9e87a6e5d6085417b2b97438ec429b7c8
Mauricio Montenegro <m.b.mont17@gmail.com>
2020-07-28 15:33:26 -0300

    Secendo day

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Logo from '../../assets/img/logo-montflix.png';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/logo-vegflix.png';
 import './Menu.css';
 import Button from '../Button'
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Montflix logo"/>
-            </a>
+            <Link to="/">
+                <img className="Logo" src={Logo} alt="Vegflix logo"/>
+            </Link>
             
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
